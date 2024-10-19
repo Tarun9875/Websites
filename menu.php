90952e100c433dc67e2a29f15c3f115c73ebdc2f
Tarun9875 <tarunpatel512005@gmail.com>
2024-10-19 21:34:41 +0530

    Add files via upload
     partation

---
+++ menu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiffin Delivery </title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/styless.css">
+    <link rel="stylesheet" href="css/sty.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logoe.png" alt="Tiffin Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="customer/login.php">Login/Registration</a>
+                    </li>
+                  
+                    <li>
+                        <a href="about-us.php">About Us</a>
+                    </li>
+                    <li>
+                        <a href="contact-us.php">Contact</a>
+                    </li>
+                    <li>
+                        <a href="../admin/login.php">Admin login</a> 
+                    </li>
+                  
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
